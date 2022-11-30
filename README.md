5a08f2313908eb2c29e43215cd74ad05c170f931
DawidJasko <68279366+DawidJasko@users.noreply.github.com>
2022-11-30 23:31:00 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# useLeyoutEffect-
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-n62t8f)
---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useLayoutEffect, useState } from 'react';
+
+import Triangle from './Triangle';
+
+import './style.css';
+
+const App = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const handleOnClick = () => setIsVisible((prev) => !prev);
+
+  const triangleComponent = isVisible ? <Triangle /> : null;
+
+  return (
+    <div>
+      <button onClick={handleOnClick}> Toggle</button>
+      {triangleComponent}
+    </div>
+  );
+};
+
+export default App;
